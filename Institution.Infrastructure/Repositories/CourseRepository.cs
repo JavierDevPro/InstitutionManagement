860a74fd1eccdd191a008ee97ca47b5c5cee7014
JavierDevPro <gjangsam@gmail.com>
2025-10-29 11:19:23 -0500

    Crud on repositories for professor and errors on CourseRepository

--- Institution.Infrastructure/Repositories/CourseRepository.cs
+++ Institution.Infrastructure/Repositories/CourseRepository.cs
@@ -1,6 +1,54 @@
+using Institution.Domain.Entities;
+using Institution.Domain.Interfaces;
+using Institution.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Institution.Infrastructure.Repositories;
 
-public class CourseRepository
+public class CourseRepository : IRepository<Course>
 {
-    
+    private readonly AppDbContext _context;
+
+    public CourseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Course>> GetAll()
+    {
+        var courses = await _context.Courses.ToListAsync();
+        return courses;
+    }
+
+    public async Task<Course> Create(Course entity)
+    {
+        var course = _context.Courses.Add(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public Task<Course> Update(int Id, Course entity)
+    {
+
+        return null
+    }
+
+    public async Task<bool> AlreadyExist(int Id)
+    {
+        var request = await 
+    }
+
+    public Task<bool> Delete(int Id)
+    {
+        throw new NotImplementedException();
+    }
+    public async Task<Course> ShowDetail(Course entity)
+    {
+        var course = await _context.Courses
+            .Include(c => c.Professor)
+            .Include(c => c.Status)
+            .FirstOrDefaultAsync(c => c == entity);
+
+        return course;
+    }
 }
--- Institution.Infrastructure/Repositories/ProfessorRepository.cs
+++ Institution.Infrastructure/Repositories/ProfessorRepository.cs
@@ -1,6 +1,57 @@
+using Institution.Domain.Entities;
+using Institution.Domain.Interfaces;
+using Institution.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Institution.Infrastructure.Repositories;
 
-public class ProfessorRepository
+public class ProfessorRepository : IRepository<Professor>
 {
+    private readonly AppDbContext _context;
+
+    public ProfessorRepository(AppDbContext context)
+    {
+        _context = context;
+    }
     
+    public async Task<IEnumerable<Professor>> GetAll()
+    {
+        return await _context.Professors.ToListAsync();
+    }
+
+    public async Task<Professor> Create(Professor entity)
+    {
+        var Professor = _context.Professors.Add(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<Professor> Update(int Id, Professor entity)
+    {
+        var existingProfessor = _context.Professors.FirstOrDefault(s => s.Id == Id);
+        if (existingProfessor == null) return null;
+        
+        existingProfessor.Name = entity.Name;
+        existingProfessor.LastName = entity.LastName;
+        existingProfessor.Document = entity.Document;
+        existingProfessor.Email = entity.Email;
+        existingProfessor.IsActive = entity.IsActive;
+        existingProfessor.PhoneNumber = entity.PhoneNumber;
+        existingProfessor.DateUpdate = entity.DateUpdate;
+        existingProfessor.Speciality = entity.Speciality;
+        
+        _context.Professors.Update(existingProfessor);
+        
+        await _context.SaveChangesAsync();
+        return existingProfessor;
+    }
+
+    public async Task<bool> Delete(int Id)
+    {
+        var existingProfessor = _context.Professors.FirstOrDefault(s => s.Id == Id);
+        if (existingProfessor == null) return false;
+        _context.Professors.Remove(existingProfessor);
+        await _context.SaveChangesAsync();
+        return true;
+    }
 }
