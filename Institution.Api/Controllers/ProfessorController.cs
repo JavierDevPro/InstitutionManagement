fd6481545644832e072685b4ef86c5e4b5bb2fa0
JavierDevPro <gjangsam@gmail.com>
2025-11-04 11:20:28 -0500

    Professor full Controller

--- Institution.Api/Controllers/ProfessorController.cs
+++ Institution.Api/Controllers/ProfessorController.cs
@@ -15,14 +15,32 @@ public class ProfessorController: ControllerBase
         _service = service;
     }
 
+    [HttpGet]
     public async Task<ActionResult<IEnumerable<ProfessorDto>>> GetAll()
     {
         var result = await _service.GetAll();
         return Ok(result);
     }
 
+    [HttpPost]
     public async Task<ActionResult<ProfessorDto>> Create(ProfessorCreateDto professor)
     {
-        
+        var result = await _service.Create(professor);
+        return (result != null) ? Created($"/{result.Id}", result) : NoContent();
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult<ProfessorDto>> Update(int id, ProfessorUpdateDto professor)
+    {
+        var result = await _service.Update(id, professor);
+        if (result == null) return NotFound();
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<bool>> Delete(int id)
+    {
+        var result = await _service.Delete(id);
+        return (result != null) ? result : NotFound();
     }
 }
